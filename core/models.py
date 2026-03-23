814319f83a6d944475fd1a0640c79f6ca93c9fd7
Данила Сысоев <crocodil-msu@ya.ru>
2026-03-23 10:07:13 +0300

    Config postgresql and create Book and Author models

--- core/models.py
+++ core/models.py
@@ -1,1 +1,29 @@
-# Create your models here.
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    publish_year = models.PositiveIntegerField(null=False, blank=False)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books"
+    )
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f'{self.author}: "{self.name}"'
