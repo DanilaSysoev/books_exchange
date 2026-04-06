546590dde8eb3f49fc5c8483f8e95d23781182b2
Данила Сысоев <crocodil-msu@ya.ru>
2026-04-06 09:43:04 +0300

    Add book detail page

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from core.views import about, index
+from core.views import about, book_detail, index
 
 app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("book/<int:book_id>/", book_detail, name="book_detail"),
 ]
